5a4165ee66abccad8304aaee0a01c4925f8b866c
skeri123 <dugalice@gmail.com>
2021-06-01 18:33:53 +0200

    Started a new improved layout and design

---
+++ registrationsystem/components/Sidebar.js
@@ -0,0 +1,138 @@
+/*eslint-disable*/
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import CLink from "./CLink";
+import Link from "next/link";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Icon from "@material-ui/core/Icon";
+import { useRouter } from "next/router";
+import AdminNavbarLinks from "./Navbars/AdminNavbarLinks.js";
+import styles from "../assents/jss/material-dashboard-react/components/sidebarStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Sidebar(props) {
+  const classes = useStyles();
+  let location = useRouter();
+  // verifies if routeName is the one active (in browser input)
+  function activeRoute(routeName) {
+    return location.pathname === routeName;
+  }
+  const { color, logoText, routes } = props;
+  var links = (
+    <List className={classes.list}>
+      {routes.map((prop, key) => {
+        var activePro = " ";
+        var listItemClasses;
+        if (prop.path === "/LogOut") {
+          activePro = classes.activePro + " ";
+          listItemClasses = classNames({
+            [" " + classes[color]]: true,
+          });
+        } else {
+          listItemClasses = classNames({
+            [" " + classes[color]]: activeRoute(prop.path),
+          });
+        }
+        const whiteFontClasses = classNames({
+          [" " + classes.whiteFont]: activeRoute(prop.path),
+        });
+        return (
+          <Link
+            href={prop.path}
+            className={activePro + classes.item}
+            activeClassName="active"
+            key={key}
+          >
+            <ListItem button className={classes.itemLink + listItemClasses}>
+              {typeof prop.icon === "string" ? (
+                <Icon
+                  className={classNames(classes.itemIcon, whiteFontClasses)}
+                >
+                  {prop.icon}
+                </Icon>
+              ) : (
+                <prop.icon
+                  className={classNames(classes.itemIcon, whiteFontClasses)}
+                />
+              )}
+              <ListItemText
+                primary={props.rtlActive ? prop.rtlName : prop.name}
+                className={classNames(classes.itemText, whiteFontClasses)}
+                disableTypography={true}
+              />
+            </ListItem>
+          </Link>
+        );
+      })}
+    </List>
+  );
+  var brand = (
+    <div className={classes.logo}>
+      <a
+        href="https://www.ius.edu.ba"
+        className={classes.logoLink}
+        target="_blank"
+      >
+        <div className={classes.logoImage}>
+          <img src="/IUSlogo2.png" alt="logo" className={classes.img} />
+        </div>
+        {logoText}
+      </a>
+    </div>
+  );
+  return (
+    <div>
+      <Hidden mdUp implementation="css">
+        <Drawer
+          variant="temporary"
+          anchor={props.rtlActive ? "left" : "right"}
+          open={props.open}
+          classes={{
+            paper: classNames(classes.drawerPaper),
+          }}
+          onClose={props.handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>
+            <AdminNavbarLinks />
+            {links}
+          </div>
+          {/*  */}
+          <div
+            className={classes.background}
+            style={{ backgroundImage: "url(/ius2.jpg)" }}
+          />
+          {/*  */}
+        </Drawer>
+      </Hidden>
+      <Hidden smDown implementation="css">
+        <Drawer
+          anchor={props.rtlActive ? "right" : "left"}
+          variant="permanent"
+          open
+          classes={{
+            paper: classNames(classes.drawerPaper),
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>{links}</div>
+          <div
+            className={classes.background}
+            style={{ backgroundImage: "url(/ius2.jpg)" }}
+          />
+        </Drawer>
+      </Hidden>
+    </div>
+  );
+}
