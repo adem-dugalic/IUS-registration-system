5cfcba810eee339b2060aa883b25f7f6bdc7c87a
skeri123 <dugalice@gmail.com>
2021-06-02 15:25:00 +0200

    New layout, notifications and func fixesa

--- registrationsystem/components/Navbars/AdminNavbarLinks.js
+++ registrationsystem/components/Navbars/AdminNavbarLinks.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
@@ -18,9 +18,42 @@ import Search from "@material-ui/icons/Search";
 // core components
 import CustomInput from "../CustomInput/CustomInput";
 import Button from "../CustomButtons/Button.js";
-import useWindowSize from "../Hooks/useWindowSize";
+//import useWindowSize from "../Hooks/useWindowSize";
 
 import styles from "../../assents/jss/material-dashboard-react/components/headerLinksStyle.js";
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== "undefined") {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
 
 const useStyles = makeStyles(styles);
 
