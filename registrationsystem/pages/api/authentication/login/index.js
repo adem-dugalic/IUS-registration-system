9c21ec6f01154671b5bc23c80c13679ea1a84777
skeri123 <dugalice@gmail.com>
2021-06-10 12:37:04 +0200

    Added woking login and reworked design

--- registrationsystem/pages/api/authentication/login/index.js
+++ registrationsystem/pages/api/authentication/login/index.js
@@ -1,82 +1,84 @@
-import dbConnect from '../../../../utilities/dbConnect';
-import User from '../../../../models/User';
-import UserSession from '../../../../models/UserSession';
-import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import auth from '../../middleware/auth'
+import dbConnect from "../../../../utilities/dbConnect";
+import User from "../../../../models/User";
+import UserSession from "../../../../models/UserSession";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import auth from "../../middleware/auth";
 
-const MongoClient = require('mongodb').MongoClient;
-const jwtSecret = 'SUPERSECRETE20220';
+const MongoClient = require("mongodb").MongoClient;
+const jwtSecret = "SUPERSECRETE20220";
 
 const saltRounds = 10;
-const url = 'mongodb://localhost:27017';
-const dbName = 'IUSSystem';
+const url = "mongodb://localhost:27017";
+const dbName = "IUSSystem";
 
 const client = new MongoClient(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
 dbConnect();
 
 export default (req, res) => {
-    if (req.method === 'POST') {
-        User.findOne({userID: req.body.userID}).then((user) => {
-            if(!user){
-                return res.status(404).json({
-                    message: "User not found!",
-                    error: new Error("User not found!")
+  if (req.method === "POST") {
+    User.findOne({ userID: req.body.userID })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({
+            message: "User not found!",
+            error: new Error("User not found!"),
+          });
+        }
+        if (user.validPassword(req.body.password)) {
+          const token = jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            expiresIn: "24h",
+          });
+
+          UserSession.replaceOne(
+            { userId: user._id },
+            { userId: user._id, token: token },
+            { upsert: true }
+          )
+            .then((session) => {
+              if (user.isAdmin) {
+                res.status(200).json({
+                  userId: user._id,
+                  token: token,
+                  expiresIn: 1,
+                  isAdmin: true,
                 });
-            }
-            if(user.validPassword(req.body.password)){
-                const token = jwt.sign({userId: user._id }, "RANDOM_TOKEN_SECRET", {
-                    expiresIn: "24h",
+              } else if (user.isSAO) {
+                res.status(200).json({
+                  userId: user._id,
+                  token: token,
+                  expiresIn: 1,
+                  isSAO: true,
                 });
-    
-                UserSession.replaceOne(
-                    {userId: user._id},
-                    {userId: user._id, token: token},
-                    {upsert: true}
-                ).then((session) => {
-                    if (user.isAdmin) {
-                      res.status(200).json({
-                        userId: user._id,
-                        token: token,
-                        expiresIn: 1,
-                        isAdmin: true,
-                      });
-                    } else if (user.isSAO) {
-                        res.status(200).json({
-                          userId: user._id,
-                          token: token,
-                          expiresIn: 1,
-                          isSAO: true,
-                        });
-                    } else {
-                      res.status(200).json({
-                        userId: user._id,
-                        token: token,
-                        expiresIn: 1,
-                        isAdmin: false,
-                        name: user.name,
-                        surname: user.surname,
-                      });
-                    }
-                  }).catch((err) => {
-                    res.status(401).json("Error replaceOne: " + err);
-                  });
-    
-            }else {
-                return res.status(401).json({
-                  message: "Incorrect password",
-                  error: new Error("Incorrect password!"),
+              } else {
+                res.status(200).json({
+                  userId: user._id,
+                  token: token,
+                  expiresIn: 1,
+                  isAdmin: false,
+                  name: user.name,
+                  surname: user.surname,
                 });
               }
-            }).catch((error) => {
-                res.status(500).json({
-                  error: error,
+            })
+            .catch((err) => {
+              res.status(401).json("Error replaceOne: " + err);
             });
-        }); 
-    }
+        } else {
+          return res.status(401).json({
+            message: "Incorrect password",
+            error: new Error("Incorrect password!"),
+          });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({
+          error: error,
+        });
+      });
+  }
 };
