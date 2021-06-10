825f7efe0b245084e50511414501714abfd1dab3
Tarik Muharem <tare98muharem@gmail.com>
2021-06-10 13:01:54 +0200

    cleaning up

--- registrationsystem/pages/api/authentication/login/index.js
+++ registrationsystem/pages/api/authentication/login/index.js
@@ -1,22 +1,7 @@
 import dbConnect from '../../../../utilities/dbConnect';
 import User from '../../../../models/User';
 import UserSession from '../../../../models/UserSession';
-import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import auth from '../../middleware/auth'
-
-const MongoClient = require('mongodb').MongoClient;
-const jwtSecret = 'SUPERSECRETE20220';
-
-const saltRounds = 10;
-const url = 'mongodb://localhost:27017';
-const dbName = 'IUSSystem';
-
-const client = new MongoClient(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 
 dbConnect();
 
