f4c13fa0979070a298ab4d387cb16d8f40567b85
Tarik Muharem <tare98muharem@gmail.com>
2021-06-10 12:33:53 +0200

    token and incorrect password update

--- registrationsystem/pages/api/authentication/login/index.js
+++ registrationsystem/pages/api/authentication/login/index.js
@@ -39,14 +39,37 @@ export default (req, res) => {
                     {userId: user._id, token: token},
                     {upsert: true}
                 ).then((session) => {
-                    if(user.isAdmin){
-                        res.status(200).json({
-                            userId: user._id
-                        })
+                    if (user.isAdmin) {
+                      res.status(200).json({
+                        userId: user._id,
+                        token: token,
+                        expiresIn: 1,
+                        isAdmin: true,
+                      });
+                    } else {
+                      res.status(200).json({
+                        userId: user._id,
+                        token: token,
+                        expiresIn: 1,
+                        isAdmin: false,
+                        name: user.name,
+                        surname: user.surname,
+                      });
                     }
-                })
+                  }).catch((err) => {
+                    res.status(401).json("Error replaceOne: " + err);
+                  });
     
-            }
+            }else {
+                return res.status(401).json({
+                  message: "Incorrect password",
+                  error: new Error("Incorrect password!"),
+                });
+              }
+            }).catch((error) => {
+                res.status(500).json({
+                  error: error,
+            });
         }); 
     }
 };
